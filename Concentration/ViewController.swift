0ca99914d714baffe3eaa681fa277cb26684c913
CarterWebb <38295957+CarterWebb@users.noreply.github.com>
2018-12-07 14:30:10 -0600

    Add files via upload

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tile: Tile!
+    var tap = UITapGestureRecognizer()
     override func viewDidLoad() {
         super.viewDidLoad()
+        tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(_:)))
+        tap.numberOfTapsRequired = 1
+        tap.numberOfTouchesRequired = 1
+        tile.addGestureRecognizer(tap)
+        tile.isUserInteractionEnabled = true
+        //self.view.addSubview(view)
+        if let tile = tile
+        {
+            tile.Image(image: UIImage(named: "download")!,id: 0)
+        }
+        //Image.isUserInteractionEnabled = true
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        print("Hello World")
+    }
 
 }
 
